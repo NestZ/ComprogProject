de77ae2166b4349ed2b50bb03399710df0daee78
NestZ <44251297+NestZ@users.noreply.github.com>
2019-04-03 00:12:39 +0700

    Update main.cpp

--- MyGames/main.cpp
+++ MyGames/main.cpp
@@ -1,8 +1,8 @@
-//#include "header/James.h"
+#include "header/James.h"
 #include "header/NestZ.h"
 #include "header/Pound.h"
-//#include "header/Billy.h"
-//#include "header/Titi.h"
+#include "header/Billy.h"
+#include "header/Titi.h"
 #include <cstdlib>
 #include <ctime>
 using namespace sf;
@@ -12,15 +12,7 @@ Game game;
 int main()
 {
     srand(time(0));
-<<<<<<< HEAD
 
-=======
-
-    Game game;
-
-
-
->>>>>>> 1cf16fffc39fcb4285371f1d38cc4a7fbbe60ccd
     game.run();
 
     return 0;
