b0bdfabd71856a6926b2f7832ed85232c1ed9de3
NestZ <44251297+NestZ@users.noreply.github.com>
2019-04-03 15:13:06 +0700

    Update main.cpp

--- MyGames/main.cpp
+++ MyGames/main.cpp
@@ -1,4 +1,4 @@
-//#include "header/James.h"
+#include "header/James.h"
 #include "header/NestZ.h"
 #include "header/Pound.h"
 #include "header/Billy.h"
@@ -11,21 +11,7 @@ Game game;
 
 int main()
 {
-    int ab;
     srand(time(0));
-<<<<<<< HEAD
-    init ();
-    inputstat_object();
-    cout << chara[0].clas<<endl;
-    cout << chara[0].stat[0] << " " <<chara[0].stat[1] <<" " << chara[0].stat[2] <<" " << chara[0].stat[3]<<" " << chara[0].stat[4]<<" " << chara[0].stat[5]<<endl;
-    get_item("sword",0);
-    cout << chara[0].stat[0] << " " <<chara[0].stat[1] <<" " << chara[0].stat[2] <<" " << chara[0].stat[3]<<" " << chara[0].stat[4]<<" " << chara[0].stat[5]<<endl;
-    cin >> ab;
-    use_potion(ab,0);
-    cout << chara[0].stat[0] << " " <<chara[0].stat[1] <<" " << chara[0].stat[2] <<" " << chara[0].stat[3]<<" " << chara[0].stat[4]<<" " << chara[0].stat[5]<<endl;
-    Game game;
-=======
->>>>>>> de77ae2166b4349ed2b50bb03399710df0daee78
 
     game.run();
 
