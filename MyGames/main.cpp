cbb9e48c18e593aafc3567fc9b378fb7c305517e
Duxz <semada15@gmail.com>
2019-04-03 14:32:08 +0700

    merge files

--- MyGames/main.cpp
+++ MyGames/main.cpp
@@ -11,12 +11,19 @@ Game game;
 
 int main()
 {
+    int ab;
     srand(time(0));
-<<<<<<< HEAD
+    init ();
+    inputstat_object();
+    cout << chara[0].clas<<endl;
+    cout << chara[0].stat[0] << " " <<chara[0].stat[1] <<" " << chara[0].stat[2] <<" " << chara[0].stat[3]<<" " << chara[0].stat[4]<<" " << chara[0].stat[5]<<endl;
+    get_item("sword",0);
+    cout << chara[0].stat[0] << " " <<chara[0].stat[1] <<" " << chara[0].stat[2] <<" " << chara[0].stat[3]<<" " << chara[0].stat[4]<<" " << chara[0].stat[5]<<endl;
+    cin >> ab;
+    use_potion(ab,0);
+    cout << chara[0].stat[0] << " " <<chara[0].stat[1] <<" " << chara[0].stat[2] <<" " << chara[0].stat[3]<<" " << chara[0].stat[4]<<" " << chara[0].stat[5]<<endl;
     Game game;
 
-=======
->>>>>>> refs/remotes/origin/master
     game.run();
 
     return 0;
